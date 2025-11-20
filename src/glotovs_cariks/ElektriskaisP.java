a88e3d1e8218e766b9b31026d75c5f96906bd197
emilcks. <emils.glotovs@sk.lvt.lv>
2025-11-20 19:46:29 +0200

    Elektriskais un Ūdens pokemoni tika parveidoti

--- src/glotovs_cariks/ElektriskaisP.java
+++ src/glotovs_cariks/ElektriskaisP.java
@@ -2,12 +2,15 @@ package glotovs_cariks;
 
 import java.util.Random;
 
+// ELEKTRISKAIS POKEMONS - AUGSTA UZBRUKUMA TIPS
 public class ElektriskaisP extends Pokemons {
     private int voltaza;
+    private boolean uzlade; // Vai pokemons ir uzlādēts
 
     public ElektriskaisP(String vards, String treneris, int maxDziviba, int uzbrukumaSpeks, int aizsardziba, int voltaza) {
         super(vards, treneris, maxDziviba, uzbrukumaSpeks, aizsardziba);
         this.voltaza = voltaza;
+        this.uzlade = false;
     }
 
     @Override
@@ -18,19 +21,66 @@ public class ElektriskaisP extends Pokemons {
     @Override
     public String uzbrukt(Pokemons pretinieks) {
         Random rand = new Random();
-        int bojajumi = this.getUzbrukumaSpeks() + (voltaza / 8) + rand.nextInt(6);
-        pretinieks.sanemtBojajumus(bojajumi);
-        return this.getVards() + " izmanto zibens spērienu! Bojājumi: " + bojajumi;
+        int pamatBojajums = this.getUzbrukumaSpeks();
+        
+        // Voltāža palielina bojājumu
+        int voltazuBonus = this.voltaza / 10;
+        int izlasesBojajums = rand.nextInt(11); 
+        
+        int kopejaisBojajums = pamatBojajums + voltazuBonus + izlasesBojajums;
+        
+        // Ja pokemons ir uzlādēts, bojājums dubultojas
+        if (this.uzlade) {
+            kopejaisBojajums *= 2;
+            this.uzlade = false; 
+        }
+        
+        // 15% iespēja uz kritisko triecienu
+        if (rand.nextInt(100) < 15) {
+            kopejaisBojajums = (int)(kopejaisBojajums * 1.5);
+            pretinieks.sanemtBojajumus(kopejaisBojajums);
+            return this.getVards() + " izmanto ZIBENS SPĒRIENU! KRITISKAIS TRIECIENS! Bojājumi: " + kopejaisBojajums + " HP";
+        }
+        
+        pretinieks.sanemtBojajumus(kopejaisBojajums);
+        return this.getVards() + " izmanto elektrisko triecienu! Bojājumi: " + kopejaisBojajums + " HP";
     }
-    
+
+    // SPECIĀLĀ SPĒJA - UZLĀDĒŠANA
     public String uzladet() {
-        this.voltaza += 15;
-        if (this.voltaza > 200) this.voltaza = 200;
-        return getVards() + " uzlādē enerģiju! Voltāža: " + voltaza;
+        this.uzlade = true;
+        this.voltaza += 20;
+        
+        if (this.voltaza > 250) {
+            this.voltaza = 250; // Maksimālā voltāža
+        }
+        
+        return this.getVards() + " uzlādējas! Nākamais uzbrukums būs DIVREIZ spēcīgāks! Voltāža: " + voltaza + "V";
+    }
+
+    // METODE VOLTĀŽAS PĀRSLĒGŠANAI
+    public String parslegtVoltazu() {
+        Random rand = new Random();
+        int jaunaVoltaza = rand.nextInt(151) + 100;
+        
+        this.voltaza = jaunaVoltaza;
+        return this.getVards() + " pārslēdz voltāžu uz " + voltaza + "V!";
+    }
+
+    @Override
+    public void attistit() {
+        super.attistit();
+        this.voltaza += 25; // Katrs līmenis palielina voltāžu
+        if (this.voltaza > 300) this.voltaza = 300;
     }
 
     @Override
     public String toString() {
-        return super.toString() + " | Voltāža: " + voltaza + "V";
+        String uzladesStatuss = uzlade ? " (UZLĀDĒTS)" : "";
+        return super.toString() + " | Voltāža: " + voltaza + "V" + uzladesStatuss;
     }
+
+    // GETTERI
+    public int getVoltaza() { return voltaza; }
+    public boolean isUzlade() { return uzlade; }
 }
--- src/glotovs_cariks/UdensP.java
+++ src/glotovs_cariks/UdensP.java
@@ -2,11 +2,15 @@ package glotovs_cariks;
 
 import java.util.Random;
 
+// ŪDENS POKEMONS - AIZSARDZĪBAS TIPS
 public class UdensP extends Pokemons {
-    private boolean zemUdens = false; 
+    private boolean zemUdens;
+    private int izvairisanasIespeja;
 
     public UdensP(String vards, String treneris, int maxDziviba, int uzbrukumaSpeks, int aizsardziba) {
         super(vards, treneris, maxDziviba, uzbrukumaSpeks, aizsardziba);
+        this.zemUdens = false;
+        this.izvairisanasIespeja = 20; 
     }
 
     @Override
@@ -17,27 +21,88 @@ public class UdensP extends Pokemons {
     @Override
     public String uzbrukt(Pokemons pretinieks) {
         Random rand = new Random();
-        int bojajumi = this.getUzbrukumaSpeks() + rand.nextInt(5);
+        int pamatBojajums = this.getUzbrukumaSpeks();
+        int izlasesBojajums = rand.nextInt(8); // 0-7 papildus bojājums
         
-        if (zemUdens) {
-            bojajumi += 12; 
-            zemUdens = false; 
-            pretinieks.sanemtBojajumus(bojajumi);
-            return getVards() + " iznirst un negaidīti uzbrūk! Kritiski bojājumi: " + bojajumi;
-        } else {
-            pretinieks.sanemtBojajumus(bojajumi);
-            return getVards() + " izmanto ūdens strūklu. Bojājumi: " + bojajumi;
+        int kopejaisBojajums = pamatBojajums + izlasesBojajums;
+        
+        // Ja pokemons ir zem ūdens, bojājums palielinās
+        if (this.zemUdens) {
+            kopejaisBojajums += 15;
+            this.zemUdens = false; // Iznirst pēc uzbrukuma
+            pretinieks.sanemtBojajumus(kopejaisBojajums);
+            return this.getVards() + " IZNIRST UN NEGAIDĪTI UZBRŪK! Kritiski bojājumi: " + kopejaisBojajums + " HP";
         }
+        
+        pretinieks.sanemtBojajumus(kopejaisBojajums);
+        return this.getVards() + " izmanto ŪDENS STRŪKLA uzbrukumu! Bojājumi: " + kopejaisBojajums + " HP";
     }
-    
+
+    @Override
+    public void sanemtBojajumus(int bojajumi) {
+        Random rand = new Random();
+        
+        // Pārbauda izvairīšanos
+        if (rand.nextInt(100) < this.izvairisanasIespeja) {
+            // Izvairījās no bojājuma
+            return;
+        }
+        
+        // Ūdens pokemoni saņem mazāk bojājumu no elektriskajiem uzbrukumiem
+        int realieBojajumi = bojajumi;
+        
+        super.sanemtBojajumus(realieBojajumi);
+    }
+
+    // SPECIĀLĀ SPĒJA - NIRŠANA
     public String nirt() {
         this.zemUdens = true;
-        return getVards() + " ienirst ūdenī, gatavojoties uzbrukumam!";
+        this.izvairisanasIespeja += 15; // Niršana palielina izvairīšanās iespēju
+        
+        if (this.izvairisanasIespeja > 50) {
+            this.izvairisanasIespeja = 50; // Maksimālā izvairīšanās
+        }
+        
+        return this.getVards() + " ienirst ūdenī! Izvairīšanās iespēja palielināta līdz " + izvairisanasIespeja + "%";
     }
-    
+
+    // SPECIĀLĀ SPĒJA - ŪDENS AIZSARDZĪBA
+    public String udensAizsardziba() {
+        // Pagaidām palielina aizsardzību
+        return this.getVards() + " izveido ūdens aizsardzības lauku! Aizsardzība palielināta!";
+    }
+
+    @Override
+    public String dziedet() {
+        // Ūdens pokemoni dziedē efektīvāk
+        if (this.getDziviba() >= this.getMaxDziviba()) {
+            return getVards() + " ir pilnībā vesels!";
+        }
+        
+        int atjaunosana = 35 + (getLimenis() * 8); // Vairāk dziedēšanas nekā parastajiem
+        this.setDziviba(this.getDziviba() + atjaunosana);
+        
+        if (this.getDziviba() > this.getMaxDziviba()) {
+            this.setDziviba(this.getMaxDziviba());
+        }
+        
+        return getVards() + " izmanto ūdens dziedēšanas spējas! Dzīvība: " + getDziviba() + "/" + getMaxDziviba();
+    }
+
+    @Override
+    public void attistit() {
+        super.attistit();
+        this.izvairisanasIespeja += 5; // Katrs līmenis palielina izvairīšanās iespēju
+        if (this.izvairisanasIespeja > 60) this.izvairisanasIespeja = 60;
+    }
+
     @Override
     public String toString() {
-        String statuss = zemUdens ? " (Zem ūdens)" : "";
-        return super.toString() + statuss;
+        String niršanasStatuss = zemUdens ? " (ZEM ŪDENS)" : "";
+        return super.toString() + " | Izvairīšanās: " + izvairisanasIespeja + "%" + niršanasStatuss;
     }
+
+    // GETTERI
+    public boolean isZemUdens() { return zemUdens; }
+    public int getIzvairisanasIespeja() { return izvairisanasIespeja; }
 }
