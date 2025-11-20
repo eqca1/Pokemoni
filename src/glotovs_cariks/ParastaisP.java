e0aa6cba3aa271d97c0f0e824284e28f168ab44b
emilcks. <emils.glotovs@sk.lvt.lv>
2025-11-20 19:39:43 +0200

    ParastaisP

--- src/glotovs_cariks/ParastaisP.java
+++ src/glotovs_cariks/ParastaisP.java
@@ -2,10 +2,13 @@ package glotovs_cariks;
 
 import java.util.Random;
 
+// PARASTAIS POKEMONS - PAMATA TIPS
 public class ParastaisP extends Pokemons {
+    private int attistibasPunkti; // Papildu parametrs attīstībai
 
     public ParastaisP(String vards, String treneris, int maxDziviba, int uzbrukumaSpeks, int aizsardziba) {
         super(vards, treneris, maxDziviba, uzbrukumaSpeks, aizsardziba);
+        this.attistibasPunkti = 0;
     }
 
     @Override
@@ -16,8 +19,40 @@ public class ParastaisP extends Pokemons {
     @Override
     public String uzbrukt(Pokemons pretinieks) {
         Random rand = new Random();
-        int bojajumi = this.getUzbrukumaSpeks() + rand.nextInt(5); 
-        pretinieks.sanemtBojajumus(bojajumi);
-        return this.getVards() + " veic triecienu. Bojājumi: " + bojajumi;
+        // Parastajam pokemonam vienkāršs uzbrukums ar nelielu izlases bojājumu
+        int pamatBojajums = this.getUzbrukumaSpeks();
+        int izlasesBojajums = rand.nextInt(6); // 0-5 papildus bojājums
+        int kopejaisBojajums = pamatBojajums + izlasesBojajums;
+        
+        pretinieks.sanemtBojajumus(kopejaisBojajums);
+        
+        // Palielina attīstības punktus par veikto uzbrukumu
+        this.attistibasPunkti++;
+        
+        return this.getVards() + " veic parasto triecienu! Bojājumi: " + kopejaisBojajums + " HP";
+    }
+
+    // SPECIĀLĀ SPĒJA - ATSPĒKOŠS TRIECIENS
+    public String atspejosaisTrieciens() {
+        Random rand = new Random();
+        int iespeja = rand.nextInt(100);
+        
+        if (iespeja < 30) { // 30% iespēja uz atspēkošu triecienu
+            this.attistibasPunkti += 2;
+            return this.getVards() + " veic atspēkošu triecienu! Nākamais uzbrukums būs divreiz spēcīgāks!";
+        } else {
+            return this.getVards() + " mēģina veikt atspēkošu triecienu, bet neveiksmīgi!";
+        }
+    }
+
+    @Override
+    public void attistit() {
+        super.attistit();
+        this.attistibasPunkti = 0; // Resetē punktus pēc attīstības
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Attīstības punkti: " + attistibasPunkti;
     }
 }
